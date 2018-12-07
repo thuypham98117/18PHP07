77873749ff2804b7591482e83ab0563355d84f31
ThuyPham <thuypham98117@gmail.com>
2018-12-07 19:01:42 +0700

    CongTruNhanChia

--- sesion 7/CongTruNhanChia.js
+++ sesion 7/CongTruNhanChia.js
@@ -1,26 +1,26 @@
 var a , b;
 
-function tinh_tong (a, b) {
+function addition (a, b) {
     document.write('Tổng của 5 và 6 là: ' + (a + b));
     document.write('<br>');
 }
-tinh_tong (5, 6);
+addition (5, 6);
 
-function tinh_hieu (a, b) {
+function subtraction (a, b) {
 	document.write('Hiệu của 5 và 6 là: ' + (a - b));
 	document.write('<br>');
 }
-tinh_hieu (5, 6)
+subtraction (5, 6)
 
-function tinh_thuong (a, b) {
+function division (a, b) {
 	document.write('Hiệu của 5 và 6 là: ' + (a / b));
 	document.write('<br>');
 }
-tinh_thuong (5, 6)
+division (5, 6)
 
-function tinh_tich (a, b) {
+function multiplication (a, b) {
 	document.write('Hiệu của 5 và 6 là: ' + (a * b));
 	document.write('<br>');
 }
-tinh_tich (5, 6)
+multiplication (5, 6)
 
