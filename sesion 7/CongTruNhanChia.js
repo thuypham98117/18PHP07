e201cb61f84da314c5fec447f6e5f0df3949c955
ThuyPham <thuypham98117@gmail.com>
2018-12-07 18:55:22 +0700

    CongTruNhanChia

---
+++ sesion 7/CongTruNhanChia.js
@@ -0,0 +1,26 @@
+var a , b;
+
+function tinh_tong (a, b) {
+    document.write('Tổng của 5 và 6 là: ' + (a + b));
+    document.write('<br>');
+}
+tinh_tong (5, 6);
+
+function tinh_hieu (a, b) {
+	document.write('Hiệu của 5 và 6 là: ' + (a - b));
+	document.write('<br>');
+}
+tinh_hieu (5, 6)
+
+function tinh_thuong (a, b) {
+	document.write('Hiệu của 5 và 6 là: ' + (a / b));
+	document.write('<br>');
+}
+tinh_thuong (5, 6)
+
+function tinh_tich (a, b) {
+	document.write('Hiệu của 5 và 6 là: ' + (a * b));
+	document.write('<br>');
+}
+tinh_tich (5, 6)
+
